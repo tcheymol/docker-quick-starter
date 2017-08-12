19077157d87d90fe1de136fa99c16ef6b5f4f8cb
Thibaut Cheymol <thibautc@theodo.fr>
2017-08-12 22:17:05 +0200

    livereload: 🎡 Add livereload on backend with pm2

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('<h1>Welcome to the backend</h1>');
+  res.send('<h1>Welcome to the live reloading backend</h1>');
 });
 
 app.listen(3030, function () {
